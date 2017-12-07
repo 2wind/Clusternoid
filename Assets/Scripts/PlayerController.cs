ddd85b351c81d75efcfaad5a55a7e64c3f362c08
whirlwind_ko <kim.yongwook.snu@gmail.com>
2017-12-07 15:56:18 +0900

    Added basic prototyping environment. not working yet.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public List<GameObject> characters;
+
+	// Use this for initialization
+	void Awake () {
+        characters.AddRange(GetComponentsInChildren<GameObject>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
