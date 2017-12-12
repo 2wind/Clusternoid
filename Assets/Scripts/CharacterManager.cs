860c5d56455ae3a5c108df7697bf733935157246
whirlwind_ko <kim.yongwook.snu@gmail.com>
2017-12-12 17:29:10 +0900

    공격 추가를 위한 준비 완료

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour {
+
+    public Transform firingPosition;
+
+    GameObject weapon;// 일단 무기를 여기다 담고 있는다. 
+    //장기적으로는 weapon.cs로 옮겨서 enum으로 불러오는 것을 지원해야 함(그래야 다양한 종류의 무기를 지원가능)
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Attack()
+    {
+        var bullet = Instantiate(GameManager.instance.bullet, firingPosition.position, firingPosition.rotation);
+        //bullet.getGameObject<bullet>().initialize;
+        //bullet.applyforce(앞쪽으로 정해진 속도로);
+
+    }
+
+    
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public GameObject bullet;
+    public GameObject healthPack;
+
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+	
+}
