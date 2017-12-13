46754fd776a0d95e19113d2615c5fa889ebb6fb2
yulcat <alszmtm@gmail.com>
2017-12-13 21:44:12 +0900

    Object Pool 추가.

---
+++ Assets/Scripts/Effect/ObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public abstract class ObjectPool : Singleton<ObjectPool>
+{
+    protected abstract string Path();
+    string GetPath(string target) => Path() + target;
+    public readonly Dictionary<string, GameObject> originals = new Dictionary<string, GameObject>();
+    public readonly Dictionary<string, List<GameObject>> pool = new Dictionary<string, List<GameObject>>();
+
+    static GameObject GetOriginal(string target)
+    {
+        var fullPath = instance.GetPath(target);
+        if (instance.originals.ContainsKey(fullPath))
+        {
+            return instance.originals[target];
+        }
+        var original = Resources.Load<GameObject>(fullPath);
+        instance.originals.Add(target, original);
+        return original;
+    }
+
+    static void CreatePool(string target)
+    => instance.pool.Add(target, new List<GameObject>());
+
+    virtual protected GameObject ReadyObject(GameObject obj)
+    {
+        obj.SetActive(true);
+        obj.transform.SetParent(transform);
+        obj.SendMessage("Ready", SendMessageOptions.DontRequireReceiver);
+        return obj;
+    }
+
+    public GameObject this[string target] => Get(target);
+
+    public static GameObject Get(string target)
+    {
+        if (!instance.pool.ContainsKey(target)) CreatePool(target);
+        var pool = instance.pool[target];
+        var inactive = pool.FirstOrDefault(o => !o.activeSelf);
+        if (inactive != null) return instance.ReadyObject(inactive);
+        var instantiated = Instantiate<GameObject>(GetOriginal(target));
+        pool.Add(instantiated);
+        return instance.ReadyObject(instantiated);
+    }
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T instance
+    => _instance ?? (_instance = CreateNew());
+    static T _instance;
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        _instance = (T)this;
+    }
+    static T CreateNew()
+    {
+        var go = new GameObject(nameof(T));
+        return go.AddComponent<T>();
+    }
+}
