cb967a4b122dfb82063184cc9223d27879f86000
whirlwind_ko <kim.yongwook.snu@gmail.com>
2017-12-20 13:18:47 +0900

    누가 쐈는지에 따라 판정을 다르게 할 수 있도록 함

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-    public string firedFrom =  "Player";// 누가 쐈는가?
+    public string firedFrom;// 누가 쐈는가?
     public int damage = 3;
     public float bulletSpeed = 1000; // 이것도 weapon.cs로 빼서 각 무기마다 속성이 달라야 한다.
 
@@ -15,26 +15,27 @@ public class Bullet : MonoBehaviour {
     // 언젠가는 enum으로 넘어가야 한다.
 
 
-    public void Initialize()
+    public void Initialize(string tag)
     {
         // 아직은 아무것도 안하지만
         // firedFrom이나 대미지 등을 어딘가에서 불러와서?
         // 결정해준다.
         GetComponent<Rigidbody2D>().velocity = (transform.up * bulletSpeed);
+        firedFrom = tag;
     }
 
     // OnTriggerEnter2D는 Collider2D other가 트리거가 될 때 호출됩니다(2D 물리학에만 해당).
     void OnTriggerEnter2D(Collider2D collision)
     {
         // 일단 플레이어만 쏜다고 가정하고 만들어보자. 
-        if (collision.gameObject.CompareTag("Player"))
+        if (firedFrom.Equals("Player") && collision.gameObject.CompareTag("Player"))
         {
             // do nothing just pass
             return;
         }
         else
         {
-            if (collision.gameObject.CompareTag("enemy"))
+            if (collision.gameObject.CompareTag("enemy") || collision.gameObject.CompareTag("Player"))
             {
                 //do some damage
                 //collision.gameObject.
