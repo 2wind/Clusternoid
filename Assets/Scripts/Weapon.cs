4d679469eba30ced3e72ddf8a1ae17d248e963a0
whirlwind_ko <kim.yongwook.snu@gmail.com>
2017-12-20 13:18:59 +0900

    AI와 무기 cs 파일 생성

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public int damage = 3;
+    public float bulletSpeed = 1000; // 이것도 weapon.cs로 빼서 각 무기마다 속성이 달라야 한다.
+                                     // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
