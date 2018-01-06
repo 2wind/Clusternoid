7af58bddaa6d5f604b1bf09a00b41809e7c25280
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-01-06 19:12:05 +0900

    BulletPool 구현

---
+++ Assets/Scripts/BulletPool.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPool : ObjectPool
+{
+    protected override string Path()
+    => "Bullets/";
+}
