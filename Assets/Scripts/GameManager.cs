f45195d0d57da4874eaae25f5ebff2cf9b3ef449
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-01-06 15:22:53 +0900

    중복되는 싱글턴 구조를 상속 구현으로 바꿈

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,25 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : Singleton<GameManager> {
 
-    public static GameManager instance = null;
     public GameObject bullet;
     public GameObject healthPack;
     public GameObject player; //현재 플레이어. 나중에 코옵을 지원하게 되면 리스트가 되겠지?
 
-
-	// Use this for initialization
-	void Awake () {
-        if (instance == null)
-            instance = this;
-
-        else if (instance != this)
-            Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject);
-    }
-	
     public static Quaternion RotationAngle(Vector2 from, Vector2 to)
     {
         Vector2 from2to = to - from;
