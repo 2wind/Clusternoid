bf237440aa33c43e11b824f6a2286888a06be189
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-01-06 17:26:35 +0900

    불필요한 player 레퍼런스 삭제

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager> {
 
     public GameObject bullet;
-    public GameObject healthPack;
-    public GameObject player; //현재 플레이어. 나중에 코옵을 지원하게 되면 리스트가 되겠지?
+    public GameObject healthPack; 
+    // TODO: ObjectPool.cs를 구현해서 거기에서 들고 있고(BulletPool.cs? 등?)
+    // Instantiate() 하던 부분을 모두 Get()을 활용하도록 바꾸기
 
     public static Quaternion RotationAngle(Vector2 from, Vector2 to)
     {
