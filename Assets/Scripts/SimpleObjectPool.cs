8be953cb10bf7486919e238b5f5da29507b68107
Lee, Kyuho <modulo641@gmail.com>
2018-01-06 20:00:03 +0900

    Simple object pool for System.Object

---
+++ Assets/Scripts/SimpleObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Clusternoid
+{
+	public class SimpleObjectPool<T> where T : new()
+	{
+		private Stack<T> pool = new Stack<T>();
+
+		public T Get()
+		{
+			if (pool.Count > 0)
+				return pool.Pop();
+			else
+				return new T();
+		}
+
+		public void Return(T obj)
+		{
+			pool.Push(obj);
+		}
+
+		public void SimpleObject(int initialSize = 10)
+		{
+			for (int i = 0; i < initialSize; i++)
+			{
+				var obj = new T();
+				pool.Push(obj);
+			}
+		}
+	}
+}
