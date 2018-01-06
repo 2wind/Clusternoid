aaeaa4de2b00080d8ec1b87e5479f03d306996bc
yulcat <alszmtm@gmail.com>
2018-01-06 21:53:48 +0900

    공격판정에 필요한 기능들 추가

---
+++ Assets/Scripts/Health/AttackListener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public struct Attack
+{
+    public readonly int attackerTag;
+    public readonly Guid id;
+    public readonly float damage;
+    public readonly float knockback;
+    public readonly float refreshTime;
+
+    public Attack(int tag, float damage, float knockback, float refresh)
+    {
+        attackerTag = tag;
+        this.damage = damage;
+        this.knockback = knockback;
+        refreshTime = refresh;
+        id = Guid.NewGuid();
+    }
+}
+
+[UsedImplicitly]
+[SuppressMessage("ReSharper", "UnassignedField.Global")]
+public class AttackData
+{
+    public float damage;
+    public float knockback;
+    public bool repeat;
+    public float refreshTime;
+}
+
+public class AttackListener : MonoBehaviour
+{
+    int tagHash;
+    public AttackData attackData;
+    public Action onAttackSuccess;
+
+    void Start()
+    {
+        tagHash = gameObject.tag.GetHashCode();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) => TriggerCaller(other);
+    void OnTriggerStay2D(Collider2D other) => TriggerCaller(other);
+
+    void TriggerCaller(Collider2D other)
+    {
+        var success = other.GetComponent<HitListener>()?.TriggerListener(new Attack(tagHash, attackData.damage,
+            attackData.knockback, attackData.repeat ? attackData.refreshTime : float.PositiveInfinity));
+        if (success.HasValue && success.Value) onAttackSuccess?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    /// <summary>
+    /// 공격판정을 당했을 때 호출되는 함수. 공격이 실제로 피해를 입혔다면 true
+    /// </summary>
+    public bool GetAttack(Attack attack)
+    {
+        // TODO : 공격을 받아서 피가 달든 넉백을 당하든 알아서 할 것
+        return true;
+    }
+}
