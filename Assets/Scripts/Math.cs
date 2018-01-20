628ba0c02857a72ebd97f18fcd859bbb57d50af9
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-01-20 16:43:10 +0900

    가우시안 분포에 의한 탄퍼짐 구현

--- Assets/Scripts/Math.cs
+++ Assets/Scripts/Math.cs
@@ -34,6 +34,16 @@ namespace Clusternoid
             return origin + offset;
         }
 
+        public static float GenerateNormalRandom(float mu, float sigma)
+        {
+            float rand1 = Random.Range(0.0f, 1.0f);
+            float rand2 = Random.Range(0.0f, 1.0f);
+
+            float n = Mathf.Sqrt(-2.0f * Mathf.Log(rand1)) * Mathf.Cos((2.0f * Mathf.PI) * rand2);
+
+            return (mu + sigma * n);
+        }
+
     }
 
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,7 +10,7 @@ public class Weapon : MonoBehaviour
     public int damage = 3;
     public float bulletSpeed = 100; // 이것도 weapon.cs로 빼서 각 무기마다 속성이 달라야 한다.
     public float cooldown = 1f; //무기의 쿨다운
-    public float spread; //무기의 탄 퍼짐 가능성. 0이면 언제나 정확함. 
+    public float spread; //무기의 탄 퍼짐 가능성. 0이면 언제나 정확함. = 확률분포의 sigma값.
     float timer;
 
     private void Awake()
@@ -31,13 +31,14 @@ public class Weapon : MonoBehaviour
     {
         //TODO: 부하 감소를 위해 IEnumerator으로 구현하기
         //TODO: 무기 종류&탄약 종류에 따라 다양한 총알 발사하기
-        //var bullet = BulletPool.Get("bullet");
-        //bullet.transform.position = firingPosition.position;
-        //bullet.transform.rotation = firingPosition.rotation;
-        //bullet.GetComponent<Bullet>().Initialize(gameObject.tag, bulletSpeed);
+        
         firingPosition.gameObject.GetComponent<AudioSource>().Play();
+        var spreadAngle = Math.GenerateNormalRandom(0, spread) * Mathf.PI;
 
-        Shooters.instance.GetShooter(gameObject.layer).Shoot(firingPosition.position, firingPosition.up);
+        Shooters.instance.GetShooter(gameObject.layer).Shoot(
+            firingPosition.position,
+            (firingPosition.up + new Vector3(Mathf.Sin(spreadAngle), 0, 0)).normalized
+            );
         
     }
 
