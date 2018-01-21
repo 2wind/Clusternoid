bd95a8b58e0b293456dbdb309255d86c22bfa42b
yulcat <alszmtm@gmail.com>
2018-01-21 11:18:53 +0900

    캐릭터 애니메이션 적용.

--- Assets/Scripts/Animation/AnimationAction.cs
+++ Assets/Scripts/Animation/AnimationAction.cs
@@ -3,7 +3,8 @@
 [RequireComponent(typeof(Animator))]
 public class AnimationAction : MonoBehaviour
 {
-    
+    GameObject root;
+
     // Use this for initialization
     void Start()
     {
@@ -12,6 +13,7 @@ public class AnimationAction : MonoBehaviour
 
     void SetAnimatorAction()
     {
+        root = GetComponentInParent<Rigidbody2D>().gameObject;
         var anim = GetComponent<Animator>();
         var behaviours = anim.GetBehaviours<AnimatorBehaviour>();
         foreach (var behaviour in behaviours)
@@ -25,10 +27,10 @@ public class AnimationAction : MonoBehaviour
         switch (action.type)
         {
             case AnimatorBehaviour.ActionType.Fire:
-                GetComponent<Weapon>().SendMessage("Fire");
+                root.GetComponent<Weapon>().SendMessage("Fire");
                 break;
             case AnimatorBehaviour.ActionType.Rotate:
-                GetComponent<Rigidbody2D>()?.AddTorque(GetComponent<Turret>().Rotation * 1f);
+                root.GetComponent<Rigidbody2D>()?.AddTorque(GetComponent<Turret>().Rotation * 1f);
                 //transform.rotation = Clusternoid.Math.RotationAngle(transform.position, PlayerController.groupCenter.transform.position);
                 break;
             case AnimatorBehaviour.ActionType.MoveForward:
@@ -44,11 +46,11 @@ public class AnimationAction : MonoBehaviour
                 Debug.Log("Action not implemented!!" + " Action name: " + action.type);
                 break;
             case AnimatorBehaviour.ActionType.Death:
-                GetComponent<DropItem>()?.Drop();
+                root.GetComponent<DropItem>()?.Drop();
                 Destroy(gameObject, action.value);
                 break;
             case AnimatorBehaviour.ActionType.ChooseRotation:
-                GetComponent<Turret>()?.ChooseRotation();
+                root.GetComponent<Turret>()?.ChooseRotation();
                 break;
             default:
                 Debug.Log("Action not implemented!!" + " Action name: " + action.type);
--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -9,15 +9,12 @@ using UnityEngine.Rendering;
 /// 플레이어가 조작하는 다수의 캐릭터들 하나 하나가 들고 있는 스크립트. 
 /// </summary>
 /// 
-public class CharacterManager : MonoBehaviour {
-
-
-
-
+public class CharacterManager : MonoBehaviour
+{
     public bool isInsider;
     public bool alive;
     public float maximumDistance = 20f;
-    public float speed = 6f;            // The speed that the player will move at.
+    public float speed = 6f; // The speed that the player will move at.
     public float repulsionCollisionRadius; // Repulse all characters that are in this radius
     public float repulsionIntensity; // Intensity of repulsion.
 
@@ -25,14 +22,14 @@ public class CharacterManager : MonoBehaviour {
     //GameObject weapon;// 일단 무기를 여기다 담고 있는다. 
     //장기적으로는 weapon.cs로 옮겨서 enum으로 불러오는 것을 지원해야 함(그래야 다양한 종류의 무기를 지원가능)
 
-    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Vector3 movement; // The vector to store the direction of the player's movement.
     Rigidbody2D rb;
     Animator ani;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        ani = GetComponent<Animator>();
+        ani = GetComponentInChildren<Animator>();
         isInsider = true;
         alive = true;
         repulsionCollisionRadius = 1.5f;
@@ -53,7 +50,6 @@ public class CharacterManager : MonoBehaviour {
         ///  float step = maxSpeed * Time.deltaTime;
         //  transform.position = Vector3.MoveTowards(transform.position, destination, step);
         //  transform.rotation = GameManager.instance.player.transform.rotation;
-
     }
 
     private void FixedUpdate()
@@ -63,8 +59,8 @@ public class CharacterManager : MonoBehaviour {
             if (isInsider)
             {
                 // Store the input axes.
-                int h = (int)Input.GetAxisRaw("Horizontal");
-                int v = (int)Input.GetAxisRaw("Vertical");
+                int h = (int) Input.GetAxisRaw("Horizontal");
+                int v = (int) Input.GetAxisRaw("Vertical");
                 // TODO: 키보드 입력의 경우 int처럼 빠릿빠릿해야 하고(-1 -> 0 -> 1) 컨트롤러 입력의 경우 적절한 입력 곡선을 가져야 함
 
                 // Move the player around the scene.
@@ -83,16 +79,16 @@ public class CharacterManager : MonoBehaviour {
                 //}
                 rb.rotation = PlayerController.groupCenter.GetComponent<Rigidbody2D>().rotation;
                 //rb.MoveRotation(PlayerController.groupCenter.GetComponent<Rigidbody2D>().rotation);
-
             }
-            else if (Vector2.Distance(PlayerController.groupCenter.GetComponent<PlayerController>().centerOfGravityCharacter.GetComponent<Rigidbody2D>().position, rb.position) < maximumDistance)
+            else if (Vector2.Distance(
+                         PlayerController.groupCenter.GetComponent<PlayerController>().centerOfGravityCharacter
+                             .GetComponent<Rigidbody2D>().position, rb.position) < maximumDistance)
             {
                 var direction = Vector2.zero;
                 direction = PathFinder.GetAcceleration(transform.position);
                 rb.MovePosition(rb.position + direction.normalized * Time.deltaTime * speed);
             }
         }
-        
     }
 
     void Move(float h, float v)
