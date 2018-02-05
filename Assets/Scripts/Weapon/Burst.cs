f9e74808057c4aeb5d0ef249ac9874c922b5d455
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-05 15:12:37 +0900

    연사로봇 구현

---
+++ Assets/Scripts/Weapon/Burst.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burst : Weapon
+{
+    public int burst = 3;
+    public float delay = 0.1f;
+
+    public override void Fire() => BurstFire();
+
+    IEnumerator BurstFire()
+    {
+        for (int i = 0; i < burst; i++)
+        {
+            firingPosition.gameObject.GetComponent<AudioSource>().Play();
+            var spreadAngle = Clusternoid.Math.NextGaussian(0, spread, -45, 45);
+            var bullet = BulletPool.Get("bullet");
+            bullet.transform.position = firingPosition.position;
+            bullet.transform.rotation = firingPosition.rotation;
+            bullet.transform.Rotate(new Vector3(0, 0, spreadAngle));
+            bullet.GetComponent<Bullet>().Initialize(gameObject.tag, bulletSpeed, damage);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
