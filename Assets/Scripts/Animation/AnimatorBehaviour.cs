8cdce3991cdef0facfac40e5c73545211b907c38
LAPTOP-CGBA8N6T\345t <345t@naver.com>
2018-02-07 22:07:35 +0900

    돌진 드론 애니메이터에 액션 배정

--- Assets/Scripts/Animation/AnimatorBehaviour.cs
+++ Assets/Scripts/Animation/AnimatorBehaviour.cs
@@ -21,7 +21,8 @@ public class AnimatorBehaviour : StateMachineBehaviour
         SetSuperArmor,
         MovePath,
         Stop,
-        KeepRotating
+        KeepRotating,
+        SetCharging
     }
 
     [Serializable]
