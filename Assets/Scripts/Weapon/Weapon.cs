26120fb36471c23d9c857ec115fcaed98cab5060
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-08 19:25:42 +0900

    캐릭터 추가 시 무작위 무기가 무작위 딜레이를 가지도록 추가 구현

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -9,7 +9,13 @@ public abstract class Weapon : MonoBehaviour {
     public float bulletSpeed = 100; // 이것도 weapon.cs로 빼서 각 무기마다 속성이 달라야 한다.
     public float spread = 0.1f; //무기의 탄 퍼짐 가능성. 0이면 언제나 정확함. = 확률분포의 sigma값.
 
+    public float preShoot = 100f; //무기의 선딜 애니메이션의 재생 속도 배수. >99이면 선딜이 재생되지 않는다.
 
+    private void Start()
+    {
+        if (gameObject.CompareTag("Player"))
+            GetComponentInChildren<Animator>().SetFloat("pre-attack", preShoot);
+    }
     public abstract void Fire();
 
 }
---
+++ Assets/Scripts/Weapon/WeaponAdder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WeaponAdder : MonoBehaviour {
+
+
+    int random;
+
+	// Use this for initialization
+	void Start () {
+        random = Random.Range(0, 3);
+        switch (random)
+        {
+            case 0:
+                gameObject.AddComponent<Pistol>();
+                GetComponent<Pistol>().preShoot = Clusternoid.Math.NextGaussian(80, 1, 50, 100);
+                break;
+
+            case 1:
+                gameObject.AddComponent<Shotgun>();
+                GetComponent<Shotgun>().preShoot = Clusternoid.Math.NextGaussian(3, 1, 1.5f, 4.5f);
+                GetComponent<Shotgun>().angle = 15;
+                break;
+
+            case 2:
+                gameObject.AddComponent<Burst>();
+                GetComponent<Burst>().preShoot = Clusternoid.Math.NextGaussian(3, 1, 1.5f, 4.5f);
+                GetComponent<Burst>().burst = 3;
+                GetComponent<Burst>().delay = 0.3f;
+                break;
+
+            default:
+                gameObject.AddComponent<Pistol>();
+                GetComponent<Pistol>().preShoot = 100f;
+                Debug.LogError("WeaponAdder Random value error, random: " + random);
+                break;
+        }
+        var weapon = GetComponent<Weapon>();
+        weapon.firingPosition = transform.Find("Firing Position").transform;
+        if (weapon.firingPosition == null)
+        {
+            Debug.LogError("Firing Position not found in this object.");
+        }
+        weapon.damage = 3;
+        weapon.bulletSpeed = 50;
+        weapon.spread = 0.5f;
+        
+    }
+}
