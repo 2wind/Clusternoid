baf412f10e0fadc4ce6daef67a964e1cbc0bc569
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-14 14:36:35 +0900

    마우스 위치를 기반으로 카메라가 부드럽게 이동하도록 설정

---
+++ Assets/Scripts/UI/MouseLocation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLocation : MonoBehaviour {
+
+    Plane xyPlane;
+	// Use this for initialization
+	void Start () {
+        xyPlane = new Plane(Vector3.forward, Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+        if (xyPlane.Raycast(camRay, out distance))
+        {
+            transform.position = camRay.GetPoint(distance);
+        }
+    }
+}
