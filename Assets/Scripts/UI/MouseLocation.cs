94f7f07f6d4aaf56ca54b9cc089b1625b8376881
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-19 21:57:18 +0900

    이제 커서를 원형으로 confine함

--- Assets/Scripts/UI/MouseLocation.cs
+++ Assets/Scripts/UI/MouseLocation.cs
@@ -6,20 +6,24 @@ public class MouseLocation : MonoBehaviour {
 
     Plane xyPlane;
     float confineSize;
+    Vector3 toCenter;
 	// Use this for initialization
 	void Start () {
         xyPlane = new Plane(Vector3.forward, Vector3.zero);
         confineSize = Mathf.Min(Screen.width, Screen.height);
+        toCenter = new Vector2(Screen.width / 2, Screen.height / 2);
     }
 
     // Update is called once per frame
     void Update () {
 
         var pos = Input.mousePosition;
-        
-        pos.x = Mathf.Clamp(pos.x, confineSize * 0.1f, confineSize * 0.9f);
-        pos.y = Mathf.Clamp(pos.y, confineSize * 0.1f, confineSize * 0.9f);
-        var camRay = Camera.main.ScreenPointToRay(pos);
+        var fromCenter = pos - toCenter;
+        fromCenter = Vector3.ClampMagnitude(fromCenter, confineSize * 0.9f);
+        //pos.x = Mathf.Clamp(pos.x, confineSize * 0.1f, confineSize * 0.9f);
+        //pos.y = Mathf.Clamp(pos.y, confineSize * 0.1f, confineSize * 0.9f);
+        var newPos = fromCenter + toCenter;
+        var camRay = Camera.main.ScreenPointToRay(newPos);
         float distance;
         if (xyPlane.Raycast(camRay, out distance))
         {
