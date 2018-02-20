8ed6c6d7e75ed660c60d8ab694a2053a077e6c8f
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-20 16:11:56 +0900

    포즈 패널 구현

---
+++ Assets/Scripts/UI/PausePanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePanel : MonoBehaviour {
+
+    public GameObject pausePanel;
+    public bool isOnPause;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && !SceneLoader.instance.isMapLoading)
+        {
+            isOnPause = !isOnPause;
+        }
+        if (SceneLoader.instance.isMapLoading)
+        {
+            isOnPause = false;
+        }
+
+        pausePanel.SetActive(isOnPause);
+        if (isOnPause)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+
+
+    public void SetPause(bool pause)
+    {
+        isOnPause = pause;
+    }
+
+}
