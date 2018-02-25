5e262821b9a10b6aff8c6fef825850111196f198
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-25 18:01:12 +0900

    manager scene에 magic map loader 추가

---
+++ Assets/Scripts/Debug/MapLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapLoader : MonoBehaviour {
+
+    InputField inputField;
+    string sceneName;
+
+
+    private void Start()
+    {
+        inputField = transform.Find("InputField").GetComponent<InputField>();
+    }
+
+
+    private void OnGUI()
+    {
+        sceneName = inputField.text;
+    }
+    public void LoadScene()
+    {
+        SceneLoader.instance.LoadScene(sceneName);
+    }
+
+
+
+
+}
