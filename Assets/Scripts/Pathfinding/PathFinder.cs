2ed5dc87422d7e69d07a902669776f742e9fecad
yulcat <alszmtm@gmail.com>
2018-02-27 23:47:33 +0900

    PathFinder에 기능추가 1. 특정 position이 맵 안에 있는지 -> public static bool IsInMap(Vector2 position) 2. 맵 바깥에서 맵 안쪽 방향이 어디인지 -> GetAcceleration에서 position이 맵 바깥일시 맵 중앙을 향하는 방향을 리턴하도록 함

--- Assets/Scripts/Pathfinding/PathFinder.cs
+++ Assets/Scripts/Pathfinding/PathFinder.cs
@@ -11,30 +11,50 @@ public class PathFinder : Singleton<PathFinder>
     IEnumerator Start()
     {
         if (map == null) yield break;
-//        map.Resize(map.width * 4, map.height * 4);
-        var center = new Vector2(map.width / 2f, map.height / 2f);
         resultPixels = new Color[map.width * map.height];
         while (target == null) yield return null;
         while (true)
         {
-            var targetPosition =
-                new Vector2Int(Mathf.FloorToInt(target.position.x + center.x),
-                    Mathf.RoundToInt(target.position.y + center.y));
+            var targetPosition = WorldToPixelSpace(target.position);
             yield return
                 StartCoroutine(DistanceMapCalculator.CalculateFlowMap(map, null, targetPosition, resultPixels));
             calculated = true;
         }
     }
 
+    Vector2Int WorldToPixelSpace(Vector2 targetPosition)
+    {
+        var center = new Vector2(map.width / 2f, map.height / 2f);
+        return new Vector2Int(Mathf.FloorToInt(targetPosition.x + center.x),
+            Mathf.RoundToInt(targetPosition.y + center.y));
+    }
+
+    public static bool IsInMap(Vector2 position)
+    {
+        var pos = instance.transform.InverseTransformPoint(position);
+        var pixelPos = instance.WorldToPixelSpace(pos);
+        var map = instance.map;
+        return pixelPos.x >= 0 && pixelPos.x < map.width
+               && pixelPos.y >= 0 && pixelPos.y <= map.width;
+    }
+
     public static Vector2 GetAcceleration(Vector2 position)
     {
         if (!instance.calculated) return Vector2.zero;
+        if (!IsInMap(position)) return ToMapCenter(position);
         var center = new Vector2(instance.map.width / 2f, instance.map.height / 2f);
         var localPos = instance.transform.InverseTransformPoint(position);
         var x = Mathf.RoundToInt(localPos.x + center.x);
         var y = Mathf.RoundToInt(localPos.y + center.y);
         var color = instance.resultPixels[x + y * instance.map.width];
         return -ColorToAccel(color).normalized;
+    }
+
+    static Vector2 ToMapCenter(Vector2 position)
+    {
+        Vector2 pos = instance.transform.InverseTransformPoint(position);
+        var center = new Vector2(instance.map.width / 2f, instance.map.height / 2f);
+        return (center - pos).normalized;
     }
 
     static Vector2 ColorToAccel(Color sample)
