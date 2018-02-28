10f214d6dca532abdac78ed756cc31a5872d8cce
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-02-28 22:03:49 +0900

    파괴되는 문 히트 애니메이션 및 체력에 비례해서 빨개지도록 설정

---
+++ Assets/Scripts/Environment/DestructableDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableDoor : MonoBehaviour
+{
+    private Color _color;
+    public Renderer[] _renderers;
+
+    void start()
+    {
+        _color = Color.white;
+    }
+
+    void Update()
+    {
+        var health = GetComponent<Health>();
+        _color.r = 2 - health.currentHP / health.initialHP;
+        _color.g = health.currentHP / health.initialHP;
+        _color.b = health.currentHP / health.initialHP;
+        foreach (Renderer rend in _renderers)
+        {
+            rend.material.SetColor(rend.material.shader.name, _color);
+        }
+
+    }
+}
