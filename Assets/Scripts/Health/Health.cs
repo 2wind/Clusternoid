8ed4b4bd1a5b93e229fe1ed011572cf44e573eb3
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-03-01 19:54:38 +0900

    죽은 뒤 공격을 받으면 더 이상 죽지 않도록 설정

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -10,12 +10,14 @@ public class Health : MonoBehaviour
 
     AI ai;
     Animator ani;
+    private bool _dead;
 
     private void OnEnable()
     {
         currentHP = initialHP;
         ai = GetComponent<AI>();
         ani = GetComponentInChildren<Animator>();
+        _dead = false;
     }
 
     /// <summary>
@@ -23,20 +25,26 @@ public class Health : MonoBehaviour
     /// </summary>
     public bool GetAttack(Attack attack)
     {
-        // TODO : 공격을 받아서 피가 달든 넉백을 당하든 알아서 할 것
-        currentHP -= attack.damage;
+        if (!_dead)
+        {
+            // TODO : 공격을 받아서 피가 달든 넉백을 당하든 알아서 할 것
+            currentHP -= attack.damage;
 
-        ai?.GetAttack();
-        if (gameObject.layer.Equals(LayerMask.NameToLayer("Player")))
-        {
-            PlayerController.groupCenter.RemoveCharacter(GetComponent<Character>());
-           // ani?.SetInteger("HP", currentHP);
-            //TODO: 실제로 destroy하지는 말고, 시체는 남겨 두어야 할 것. 이를 위해 시체 로직을 만들어야 한다.
+            ai?.GetAttack();
+            if (gameObject.layer.Equals(LayerMask.NameToLayer("Player")))
+            {
+                PlayerController.groupCenter.RemoveCharacter(GetComponent<Character>());
+               // ani?.SetInteger("HP", currentHP);
+                //TODO: 실제로 destroy하지는 말고, 시체는 남겨 두어야 할 것. 이를 위해 시체 로직을 만들어야 한다.
+            }
+            else if (currentHP <= 0)
+            {
+
+                ai?.SetDeath();
+            }
+            return true;
         }
-        else if (currentHP <= 0)
-        {
-            ai?.SetDeath();
-        }
-        return true;
+
+        return false;
     }
 }
