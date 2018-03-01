6f762825643ee4ccb71e3163ca60d5e94a0d175e
SNU\gkdh753 <gkdh1015@gmail.com>
2018-03-01 18:05:31 +0900

    확률 드랍코드 조금 수정(거의 변경없음)

--- Assets/Scripts/Item/DropItem.cs
+++ Assets/Scripts/Item/DropItem.cs
@@ -5,8 +5,6 @@ using Clusternoid;
 
 public class DropItem : MonoBehaviour {
 
-    float dropChecker;
-
     public float dropPercent;
 
     public List<GameObject> list;
@@ -15,13 +13,11 @@ public class DropItem : MonoBehaviour {
     {
         foreach (var item in list)
         {
-            dropChecker = Random.value;
-
-            if(dropPercent == 0)
+            if(dropPercent < Mathf.Epsilon)
             {
                 return;
             }
-            else if(Random.value <= dropPercent/100)
+            else if(Random.value < dropPercent/100 + Mathf.Epsilon)
             {
                 Instantiate(item, Math.RandomOffsetPosition(transform.position, 0.1f), transform.rotation);
             }
