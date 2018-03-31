65e248c9ccefb08b8b320303085ec391ee7952b2
whirlwind_ko <kim.yongwook.snu@gmail.com>
2018-03-31 19:09:15 +0900

    UIManager 스크립트 추가

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Singleton<UIManager>
+{
+
+    public Queue<GameObject> activeWindows;
+
+	// Use this for initialization
+	void Start () {
+		activeWindows = new Queue<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
